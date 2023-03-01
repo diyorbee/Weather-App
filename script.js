9ada2a5c476c3ffebfd1c2f59cff38ef7d530495
shakhzod_u <mirzoitovsanjar363@gmil.com>
2023-03-01 15:40:56 +0500

    Weather-App

--- script.js
+++ script.js
@@ -25,6 +25,28 @@ function displayResults(weather) {
     city.innerHTML = `${weather.name}, ${weather.sys.country}`
 
     let now = new Date()
-    let date = document.querySelector('.date')
-    date.innerHTML = `${weather.name}, ${now}`
+    let date = document.querySelector('.location .date')
+    date.innerHTML = dataBuilder(now)
+    
+    let temp = document.querySelector('.temp')
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>` 
+
+    let cloud = document.querySelector('.weather')
+    cloud.innerHTML = `${weather.weather[0].main}`
+
+    let hi_low = document.querySelector('.hi-low')
+    hi_low.innerHTML = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`
 }
+
+function dataBuilder(s) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    let day = days[s.getDay()]   
+    let date = s.getDay()
+    let month = months[s.getMonth()]
+    let year = s.getFullYear()
+
+    return `${day}, ${date}, ${month}, ${year}`
+}
+
